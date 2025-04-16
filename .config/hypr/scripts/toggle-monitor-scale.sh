3749e212f3486c397ba0461d4139a82bab9dcb38
n1h41 <nihalninu25@gmail.com>
2025-04-17 04:32:49 +0530

    added script to toggle laptop screen scale

---
+++ .config/hypr/scripts/toggle-monitor-scale.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Path to the file
+file="/home/n1h41/dotfiles/.config/hypr/userprefs.conf"
+
+# Get the current line
+current_line=$(sed -n '13p' "$file")
+
+if [[ "$current_line" == *"auto, 2"* ]]; then
+  sed -i '13s/auto, 2/auto, auto/' "$file"
+  # sed -i '13s/auto, 2/auto, 1.33333/' "$file"
+else
+  sed -i '13s/auto, auto/auto, 2/' "$file"
+fi
